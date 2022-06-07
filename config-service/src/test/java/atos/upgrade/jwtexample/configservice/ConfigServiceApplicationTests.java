56080a6994c9452b6e1c8616ec83d1cc0b4fcd2e
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-06-07 10:20:43 -0600

    Added config server

---
+++ config-service/src/test/java/atos/upgrade/jwtexample/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package atos.upgrade.jwtexample.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

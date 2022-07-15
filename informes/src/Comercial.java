24a4501503b1a04ed35ef4b1ba5f7a756ff95869
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-07-15 11:55:05 -0600

    removed folder

--- informes/src/Comercial.java
+++
@@ -1,8 +0,0 @@
-public class Comercial implements Empleado{
-
-    @Override
-    public String presentarInforme() {
-        GenerarInforme informe = new GenerarInforme();
-        return informe.informeSemestral();
-    }
-}
--- informes/src/Informes.java
+++
@@ -1,4 +0,0 @@
-public interface Informes {
-    String informeSemestral();
-    String informeAnual();
-}
